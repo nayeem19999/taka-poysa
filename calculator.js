1f7ac0342fbae4f43a5864122bd03090e7c5462c
eshikhon222 <mern2.eshikhon@gmail.com>
2024-10-22 15:01:55 +0600

    another

--- calculator.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('submit').addEventListener('click',function(){
-    const firstInput = document.getElementById('first').value;
-    const secondInput = document.getElementById('second').value;
-    const convertedFirst = parseInt(firstInput);
-    const convertedSecond = parseInt(secondInput);
-    const result = convertedFirst + convertedSecond;
-    const span = document.getElementById('result')
-    span.innerText = result;
-})
